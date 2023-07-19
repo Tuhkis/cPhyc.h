d2a1d55c9e229974628f3a087fa45c4107f9f6a8
Tuhkis <nicolindstromj@gmail.com>
2023-07-19 18:39:18 +0300

    C++ example

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define CPHYC_IMPL
+#include "cPhyc.h"
+
+int main() {
+	cph::Rect r1 = (cph::Rect) {0, 0, 64, 64};
+	cph::Rect r2[] = { (cph::Rect) {128, 0, 64, 64} };
+
+	for ( int i = 0; i < 10; i++ ) {
+		cph::moveAndCollide(&r1, r2, 1, 16, 0);
+		printf("X Pos: %d\n", r1.x);
+		if (cph::isOnWall(r1, r2, 1))
+			printf("Is on wall!\n");
+	}
+
+	return 0;
+}
